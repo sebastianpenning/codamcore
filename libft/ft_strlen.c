3a4f9a8e23d794821331404d3792edf190c0aebd
spenning <whimizera@mailbox.org>
2023-10-04 13:06:07 +0200

    first prototype ft_strlen.c

---
+++ libft/ft_strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <unistd.h>
+#include <ctype.h>
+
+
+
+int ft_strlen(int input){
+    return(0);
+}
+
+
+int main (){
+    char test1[20] = "test1";
+    char test2[20] = "test12";
+    char test3[20] = "test123";
+
+    return(0);
+}
