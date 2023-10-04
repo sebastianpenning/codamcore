228014cd176edbfb2ce294af584b36e1b34c2e2e
spenning <whimizera@mailbox.org>
2023-10-04 10:57:12 +0200

    first prototype of ft_isprint.c

---
+++ libft/ft_isprint.c
@@ -0,0 +1,33 @@
+#include <stdio.h> 
+#include <unistd.h>
+#include <ctype.h>
+
+
+int ft_isprint(int input){
+    if (input > 31 && input < 127)
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+    
+
+
+    return(0);
+}
+
+int main (){
+    int var1 = 'c';
+    int var2 = '\n';
+    int var3 = ' ';
+    int var4 = ']';
+
+    printf("\nThe results = %d", ft_isprint(var1));
+    printf("\nThe results = %d", ft_isprint(var2));
+    printf("\nThe results = %d", ft_isprint(var3));
+    printf("\nThe results = %d", ft_isprint(var4));
+
+    return (0);
+}
