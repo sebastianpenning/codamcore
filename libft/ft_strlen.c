04fbb0a78138e16bc228f5b2bee4e6aa52da338c
spenning <whimizera@mailbox.org>
2023-10-04 14:29:52 +0200

    ft_strlen.c first version

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -1,11 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 13:57:49 by spenning          #+#    #+#             */
+/*   Updated: 2023/10/04 14:28:37 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h> 
 #include <unistd.h>
 #include <ctype.h>
 
 
 
-int ft_strlen(int input){
-    return(0);
+int ft_strlen(char input[]){
+    if (input[0] == '\0')
+    {
+        return(0);
+    }
+    else
+    {
+        int index;
+        index = 0;
+
+        while (input[index] != '\0')
+        {
+            index++;   
+        }
+        return index;
+    }
 }
 
 
@@ -13,6 +39,10 @@ int main (){
     char test1[20] = "test1";
     char test2[20] = "test12";
     char test3[20] = "test123";
+
+    printf("\nThis is the results: %d", ft_strlen(test1));
+    printf("\nThis is the results: %d", ft_strlen(test2));
+    printf("\nThis is the results: %d", ft_strlen(test3));
 
     return(0);
 }
