ddc81e51cf44841bf5bd412c05ceb5e6aff49f65
spenning <whimizera@mailbox.org>
2023-10-04 14:29:20 +0200

    ft_isalnum.c norminette aanpassingen

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -1,28 +1,42 @@
-#include <stdio.h> 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: spenning <spenning@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/04 14:00:52 by spenning          #+#    #+#             */
+/*   Updated: 2023/10/04 14:19:47 by spenning         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <ctype.h>
 
-int ft_isalnum(int input){
-    if ((input > 47 && input < 58) || (input > 64 && input < 91) || (input > 96 && input < 123))
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+int	ft_isalnum(int input)
+{
+	if ((input > 47 && input < 58) || (input > 64 && input < 91)
+		|| (input > 96 && input < 123))
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
 }
 
 
-int main (){
-    int var1 = 'c';
-    int var2 = '1';
-    int var3 = ']';
-    int var4 = '@';
+int main ()
+{
+	int var1 = 'c';
+	int var2 = '1';
+	int var3 = ']';
+	int var4 = '@';
 
-    printf("\nThe result is %d", ft_isalnum(var1));
-    printf("\nThe result is %d", ft_isalnum(var2));
-    printf("\nThe result is %d", ft_isalnum(var3));
-    printf("\nThe result is %d", ft_isalnum(var4));
-    return (0);
+	printf("\nThe result is %d", ft_isalnum(var1));
+	printf("\nThe result is %d", ft_isalnum(var2));
+	printf("\nThe result is %d", ft_isalnum(var3));
+	printf("\nThe result is %d", ft_isalnum(var4));
+	return (0);
 }
