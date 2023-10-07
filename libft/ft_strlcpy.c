3a462c7aba6091d727424e1cbde27ce6c61fcfe1
spenning <whimizera@mailbox.org>
2023-10-07 18:40:15 +0200

    first version ft_strlcpy.c

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strlcpy.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: spenning <spenning@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/06 22:35:47 by spenning      #+#    #+#                 */
+/*   Updated: 2023/10/07 18:20:30 by spenning      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> 
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
