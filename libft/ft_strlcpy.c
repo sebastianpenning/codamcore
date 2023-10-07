524f5a8672b715e387d04753a94d331c958cad4d
spenning <whimizera@mailbox.org>
2023-10-07 18:40:52 +0200

    second version ft_strlcpy.c

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/06 22:35:47 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/07 18:20:30 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/07 18:40:22 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,3 +16,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+
+}
+
+int main ()
+{
+	const char src_test1[30] = "here is the man pa defin";
+	const char src_test2[25] = " e copied into buffer ";
+	const char src_test3[20] = " ers maximum are co";
+	const char src_test4[15] = " e size of ";
+
+	char dest_test1[20];
+	char dest_test1[20];
+	char dest_test1[20];
+	char dest_test1[20];
+
+	
+	
+	return (0);
+}
