87c1fb9e9af5f44fbb74f295a1cf79567bb7e479
spenning <whimizera@mailbox.org>
2023-10-11 19:24:17 +0200

    ft_substr.c extra space for null terminator with malloc

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/10 21:36:05 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/10 21:51:16 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/11 19:22:42 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 	char * ns_cptr;
 	size_t index;
 
-	s_vptr = malloc(len);
+	s_vptr = malloc(len + 1);
 	s_cptr = (char*)s;
 	ns_cptr = (char*)s_vptr;
 	index = 0;
@@ -39,6 +39,7 @@ char *ft_substr(char const *s, unsigned int start, size_t len)
 		start++;
 		index++;
 	}
+	ns_cptr[index] = '\0';
 	return(ns_cptr);
 }
 
