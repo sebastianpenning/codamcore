5d470f1448b41d8846cf2fbdf2945d8d56d862ec
spenning <whimizera@mailbox.org>
2023-10-12 21:47:20 +0200

    split second draft

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/12 13:31:02 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/12 15:43:24 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/12 21:46:59 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,66 +18,43 @@
 #include "ft_strlcpy.c"
 #include "ft_strchr.c"
 
-static int alloc_splits(size_t sub_index, size_t ns_string_index, char * buffer, char** alloc_arr)
-{
-	size_t alloc_size;
-	
-	if (sub_index > 0)
-	{
-		buffer[sub_index] = '\0';
-		alloc_size = sizeof(char) * (ft_strlen(buffer) + 1);
+// static free all
 
-		alloc_arr[ns_string_index] = (char*)malloc(alloc_size);
-		ft_strlcpy(alloc_arr[ns_string_index], buffer, sub_index);
-		ns_string_index++;
+static int store_substring(char **allocation_array, char const *string, size_t end_index)
+{
+	char* string_cptr;
+	string_cptr=(char*)malloc(sizeof(char) * (end_index + 1));
+	allocation_array[0] = strlcpy(allocation_array + 0, string, end_index);
+}
+static int detect_substring(char **alloc_array, char const *strin, char charact)
+{
+	size_t index;
+
+	index = 1;
+	if (ft_strchr(strin, charact) == NULL)
+	{
+		while (ft_strchr(strin, charact) != NULL)
+		{
+			index++;
+		}
+		store_substring(alloc_array, strin, index);
 	}
-	return(ns_string_index);
+}	
+
+static char ** create_splits(char **alloc_arr, char const *stri, char chara)
+{
+	size_t alloc_index ;
+	alloc_index = 0; 
+	while (!stri)
+	{
+		detect_substring(alloc_arr[alloc_index], stri, chara);
+		stri++;
+		alloc_index++;
+	}
+	return(alloc_arr);
 }
 
-
-static char ** create_splits(char **alloc_arr, size_t s_len, char const *s, char c)
-{
-	char buffer[16384];
-	size_t org_index;
-	size_t sub_index;
-	size_t ns_string_index;	
-
-	org_index = 0;
-	ns_string_index = 0;
-
-	while (org_index < s_len)
-	{
-		// detect_substring
-		while (org_index < s_len)
-		{
-			if (ft_strchr(c, s[org_index]) == NULL)
-			{
-				break;
-			}
-			org_index++;
-		}
-		size_t sub_index = 0;
-
-		// store_substring
-		while (org_index < s_len)
-		{
-			if (ft_strchr(c, s[org_index]) != NULL)
-			{
-				break;
-			}
-			buffer[sub_index] = s[org_index];
-			org_index++;
-			sub_index;
-		}
-		if (ns_string_index alloc_splits(sub_index, ns_string_index, buffer, alloc_arr))
-		{
-			return(NULL);
-		}
-		return(alloc_arr);
-	}
-
-
-static size_t sumsplit(char const *s, char c)
+static size_t sumsplit(char const *str, char ch)
 {
 	size_t sum;
 	char * s_cptr;
@@ -87,7 +64,7 @@ static size_t sumsplit(char const *s, char c)
 
 	while (s_cptr[index] != '\0')
 	{
-		if (s_cptr[index] == c)
+		if (s_cptr[index] == ch)
 		{
 			sum++;
 		}
@@ -96,49 +73,16 @@ static size_t sumsplit(char const *s, char c)
 	return(sum);
 }
 
-	
-
-	return(alloc_arr);
-}
-
 size_t ft_split(char const *s, char c)
 {
 	size_t len_s;
 	size_t split_sum;
 	char ** return_arr;
 	
-	len_s = ft_strlen(s);
 	split_sum = sumsplit(s, c);
 	return_arr = (char**)malloc(sizeof(char*) * split_sum);
 
-
-	// while (index < len_s)
-	// {
-	// 	while (index < len_s)
-	// 	{
-	// 		if(ft_strchr(s, c) == NULL)
-	// 		{
-	// 			break;
-	// 		}
-	// 		index++;
-	// 	}	
-		
-	// }
-	// 	string_index = 0;
-	// 	while (index < len_s)
-	// 	{
-	// 		if(ft_strchr(s, c) != NULL)
-	// 		{
-	// 			break;
-	// 		}
-	// 		buffer[string_index] = s[index];
-	// 		string_index++;
-	// 	}
-	// 	buffer[string_index] = '\0';
-
-	// 	alloc_size = sizeof(char) * (ft_strlen(buffer) + 1);
-
-	// 	return_arr[string_index]
+	create_splits(return_arr, s, c);
 
 	return(split_sum);
 }
