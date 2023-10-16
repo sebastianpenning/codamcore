be8a8445dc45a52fee286f933c3455e6a99071b9
spenning <whimizera@mailbox.org>
2023-10-16 20:39:59 +0200

    fixed calloc werror error

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,16 +6,20 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/10 19:35:40 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/16 20:21:04 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/16 20:39:08 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
+
+//memory_vptr = NULL; because the memory should be zero
+//according to the manual
 
 #include "libft.h"
 
 void * ft_calloc(size_t nmemb, size_t size)
 {
 	size_t totalsize;
-	void * memory_vptr; 
+	void * memory_vptr;
+	memory_vptr = NULL; 
 	totalsize = nmemb * size;
 	if (size == 0 || nmemb == 0)
 	{
