848e1429f9bb8ed023cc17f88c2e5f3c8ccaca21
spenning <whimizera@mailbox.org>
2023-10-17 20:52:59 +0200

    fixed isalpha if statement

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -6,18 +6,20 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/16 20:08:13 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/16 20:21:22 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/17 20:50:53 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalpha(int c){
-    if ((c > 64 && c < 91) || (c > 96 && c < 123))
+int ft_isalpha(int c)
+{
+    c = c % 256;
+    if (((c > 64) && (c < 91)) || ((c > 96) && (c < 123)))
     {
-        return(0);
+        return(c - 47);
     }
     else{
-        return(1);
+        return(0);
     }
 }
