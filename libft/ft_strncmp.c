c5c8c0abdf7d1804f40c8a02abeb8caa708dec4b
spenning <whimizera@mailbox.org>
2023-10-17 21:16:05 +0200

    fixed strncmp 1-based index, and n = 0 issue

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/08 16:15:25 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/16 20:25:38 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/17 21:15:08 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ int ft_strncmp(const char *s1, const char *s2, size_t n)
 	char * s2_cptr;
 
 	index = 0;
+	returnval = 0;
 	s1_cptr = (char*)s1;
 	s2_cptr = (char*)s2;
-	returnval = (s1_cptr[index] - s2_cptr[index]);
-	while (index < n && s1_cptr[index] != '\0' && s2_cptr[index] != '\0')
+	if (n > 0)
+		returnval = (s1_cptr[index] - s2_cptr[index]);
+	while (index < n - 1 && s1_cptr[index] != '\0' && s2_cptr[index] != '\0')
 	{
 		index++;
 		returnval = returnval + (s1_cptr[index] - s2_cptr[index]);
