07619bca5e6e9ccce19a43900e8658509a6e1de6
spenning <whimizera@mailbox.org>
2023-10-17 20:11:09 +0200

    fixed ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,11 +6,15 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/08 18:06:38 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/16 20:22:47 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/17 20:06:35 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+// % 256 added because if a int above 256 is given it will 
+// go off the ascii table. Otherwise it can never find the 
+// char it is looking for in the memory
 
 void * ft_memchr(const void *s, int c, size_t n)
 {	
@@ -21,7 +25,7 @@ void * ft_memchr(const void *s, int c, size_t n)
 
 	while (index < n)
 	{
-		if (*(s_vptr + index) == c)
+		if (*(s_vptr + index) == (c % 256))
 		{
 			s_vptr = s_vptr + index;
 			return(s_vptr);
