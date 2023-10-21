db5d56946dcaaddd243df3b2e55d96c394ed7bce
spenning <whimizera@mailbox.org>
2023-10-21 20:04:02 +0200

    new version of ft_strncmp + norminette + docs

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,29 +6,30 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/08 16:15:25 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/17 21:15:08 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 20:03:22 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+// function compares s1 to s2 until either n is reached or both string
+// have reached their null terminator
+
 #include "libft.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t index;
-	int returnval;
-	char * s1_cptr;
-	char * s2_cptr;
+	size_t	index;
+	size_t	s1_len;
+	size_t	s2_len;
+	int		returnval;
 
 	index = 0;
 	returnval = 0;
-	s1_cptr = (char*)s1;
-	s2_cptr = (char*)s2;
-	if (n > 0)
-		returnval = (s1_cptr[index] - s2_cptr[index]);
-	while (index < n - 1 && s1_cptr[index] != '\0' && s2_cptr[index] != '\0')
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	while (index < n && (index < s1_len || index < s2_len))
 	{
+		returnval = returnval + (s1[index] - s2[index]);
 		index++;
-		returnval = returnval + (s1_cptr[index] - s2_cptr[index]);
 	}
-	return(returnval);
+	return (returnval);
 }
