3a3b553b879b18601d3b50c339eff202879344ca
spenning <whimizera@mailbox.org>
2023-10-21 15:53:20 +0200

    memcpy norminette proof

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,23 +6,21 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/06 21:30:56 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/17 20:21:46 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 15:51:25 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	long unsigned int index;
+	long unsigned int	index;
+	const char			*src_cptr;
+	char				*dest_cptr;
+
 	index = 0;
-
-	const char *src_cptr;
-	char *dest_cptr;
-
 	src_cptr = (char *)src;
 	dest_cptr = (char *)dest;
-
 	while (index < n)
 	{
 		dest_cptr[index] = src_cptr[index];
@@ -30,5 +28,3 @@ void *ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (dest_cptr);
 }
-
-
