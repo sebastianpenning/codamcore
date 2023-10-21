e5876110de90b2d91cf9935a413e6adf246c7a94
spenning <whimizera@mailbox.org>
2023-10-21 18:09:35 +0200

    ft_memset.c norminette proof

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,22 +6,23 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/04 14:32:28 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/17 20:15:48 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 18:09:04 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
-	char *cptr;
-	cptr = (char*)s;
-	long unsigned int index;
+	long unsigned int	index;
+	char				*cptr;
+
+	cptr = (char *)s;
 	index = 0;
 	while (index < n)
 	{
 		cptr[index] = (const int)c;
 		index++;
 	}
-	return(cptr);
+	return (cptr);
 }
