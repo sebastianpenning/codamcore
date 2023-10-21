4be7a14b66bd4d9ba30c066b20cd29b8f23e755c
spenning <whimizera@mailbox.org>
2023-10-21 19:37:14 +0200

    strlcpy norminette fix + docs

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,33 +6,31 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/06 22:35:47 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/20 19:48:00 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 19:36:39 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+// strlcpy always returns the length of src. And it will keep on copying
+// until it reaches size - 1, and there are still charachters to copy
+// from src. Afterwards it null terminates the result.
+
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t srclen;
-	size_t index;
-	const char *src_cptr;
-	char *dst_cptr;
+	size_t		srclen;
+	size_t		index;
 
-	src_cptr = src;
-	dst_cptr = dst;
-	srclen = ft_strlen(src_cptr);
+	srclen = ft_strlen(src);
 	index = 0;
-
 	if (size > 0)
 	{
-		while (index < (size -1) 
-		&& src_cptr[index])
-			{
-				dst[index] = src[index];
-				index ++;
-			}
+		while (index < (size -1) && src[index])
+		{
+			dst[index] = src[index];
+			index ++;
+		}
 		dst[index] = '\0';
 	}
-	return(srclen);
+	return (srclen);
 }
