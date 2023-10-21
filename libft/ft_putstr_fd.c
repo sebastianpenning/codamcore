5014040d78032f0e5a47d02e34bf2e6375a89f1f
spenning <whimizera@mailbox.org>
2023-10-21 18:17:47 +0200

    ft_putstr_fd.c norminette proof

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/12 18:44:17 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/16 20:24:23 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 18:17:24 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_putstr_fd(char* s, int fd) 
+void	ft_putstr_fd(char *s, int fd)
 {
 	write(fd, s, ft_strlen(s));
 }
