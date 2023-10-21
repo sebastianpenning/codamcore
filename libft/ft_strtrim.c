d6d91c85fdbec47d86a8b036d92d49c7b07b09ef
spenning <whimizera@mailbox.org>
2023-10-21 21:17:19 +0200

    ft_strtrim.c norminette + docs

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,51 +6,56 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/11 19:38:41 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/20 19:47:57 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 21:13:42 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+// function tries to trim certain set from the beginning with
+// is trimmable. Then it goes to the end to do the same. 
+// If all is trimmed then a string is malloced, and it is returned
+// if successfull. 
+
 #include "libft.h"
 
-static int istrimmable(char const *s1 ,char const *set)
+static int	istrimmable(char const *s1, char const *set)
 {
-	char * set_cptr;
-	char * s1_cptr;
-	set_cptr = (char*)set;
-	s1_cptr = (char*)s1;
-	while(set_cptr[0] != '\0' && s1_cptr[0] != '\0')
+	char	*set_cptr;
+	char	*s1_cptr;
+
+	set_cptr = (char *)set;
+	s1_cptr = (char *)s1;
+	while ((set_cptr[0] != '\0') && (s1_cptr[0] != '\0'))
 	{
 		if (s1_cptr[0] == set_cptr[0])
 		{
-			return(1);
+			return (1);
 		}
 		set_cptr++;
 	}
-	return(0);
+	return (0);
 }
 
-char * ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t remainder;
-	char * s1_cptr;
-	char * s_return;
-	s1_cptr = (char*)s1;
-	s_return = 0;
+	size_t	remainder;
+	char	*s1_cptr;
+	char	*s_return;
 
-	while(istrimmable(s1_cptr, set))
+	s1_cptr = (char *)s1;
+	s_return = 0;
+	while (istrimmable(s1_cptr, set))
 	{
 		s1_cptr++;
 	}
 	remainder = ft_strlen(s1_cptr);
-	while(istrimmable((s1_cptr + remainder) - 1, set) && remainder > 0)
+	while (istrimmable((s1_cptr + remainder) - 1, set) && remainder > 0)
 	{
 		remainder--;
 	}
 	remainder = remainder + 1;
-	s_return = (char*)malloc(remainder);
+	s_return = (char *)malloc(remainder);
 	if (s_return == NULL)
-		return(NULL);
+		return (NULL);
 	ft_strlcpy(s_return, s1_cptr, remainder);
-	return(s_return);
+	return (s_return);
 }
-
