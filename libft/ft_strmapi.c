3f4d917527be64a5e2e434a17902cc18dfbcfa50
spenning <whimizera@mailbox.org>
2023-10-21 19:44:26 +0200

    norminette proof + docs ft_strmapi.c

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,25 +6,27 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/15 20:21:09 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/16 20:25:32 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/21 19:44:04 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+// this function created a new string based on s, and then applies f
+// function to every character in the new string that is based on s.
+
 #include "libft.h"
 
-char * ft_strmapi(char const *s, char (*f) (unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
 {
-	int index;
-	size_t s_len;
-	char *new_string;
-	char r_char;
+	int		index;
+	size_t	s_len;
+	char	*new_string;
+	char	r_char;
 
 	s_len = ft_strlen(s);
-	new_string = (char*)malloc(s_len + 1);
+	new_string = (char *)malloc(s_len + 1);
 	index = 0;
-	
 	if (new_string == NULL)
-		return(NULL);
+		return (NULL);
 	while (s[index] != '\0')
 	{
 		r_char = f(index, s[index]);
@@ -32,5 +34,5 @@ char * ft_strmapi(char const *s, char (*f) (unsigned int, char))
 		index++;
 	}
 	new_string[index] = '\0';
-	return(new_string);
+	return (new_string);
 }
