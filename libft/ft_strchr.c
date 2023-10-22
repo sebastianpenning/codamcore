fb28ebc0de7b00319ca25f675ef47b5b0db4261d
spenning <whimizera@mailbox.org>
2023-10-22 16:02:22 +0200

    unsigned char fix strchr

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/08 12:50:37 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/22 15:57:26 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/22 16:01:58 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_strchr(const char *s, int c)
 	s_cptr = (char *)s;
 	while (s_cptr[index] != '\0')
 	{
-		if (s_cptr[index] == (unsigned char)c)
+		if ((unsigned char)s_cptr[index] == (unsigned char)c)
 		{
 			return_cptr = &s_cptr[index];
 			return (return_cptr);
