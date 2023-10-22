6c20fef34ac7b10862616f674c6db807e5f40a13
spenning <whimizera@mailbox.org>
2023-10-22 14:03:29 +0200

    libft.h docs added

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,21 @@
 /*   By: spenning <spenning@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/21 23:03:23 by spenning      #+#    #+#                 */
-/*   Updated: 2023/10/21 23:48:08 by spenning      ########   odam.nl         */
+/*   Updated: 2023/10/22 14:02:53 by spenning      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+// These are header guards that prevent the double definitions in the 
+// source file. This prevents library dependency from double copying
+// libraries. if libft is already defined if won't define it again.
+// name is LIBFT_H because the name should be the full filename of the 
+// header file, in all caps, with underscores for spaces and ​punctuation.
+
 #ifndef LIBFT_H
 # define LIBFT_H
+
+//these inclusions are needed to be able to use the write function from
+// unistd library. and malloc and free from the stdlib library.
 
 # include <stdlib.h>
 # include <unistd.h>
