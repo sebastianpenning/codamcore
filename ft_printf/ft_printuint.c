3a6276ddacf707f62066968ff59c83f793e36d48
spenning <whimizera@mailbox.org>
2023-11-23 16:39:32 +0100

    unsigned int format file added

--- ft_printf/ft_printuint.c
+++ ft_printf/ft_printuint.c
@@ -1,12 +1,27 @@
 #include "libftprintf.h"
 
+
+static void printunit (unsigned int arg)
+{
+	if (arg == 0)
+		ft_putchar_fd('0', 1);
+	if(arg > 0)
+	{
+		if(arg > 9)
+			printunit(arg / 10);
+		ft_putchar_fd(48 + (arg % 10), 1);
+	}
+}
+
 int ft_printuint(va_list args)
 {
 	int rtr_va_arg;
 	unsigned int arg;
 
 	rtr_va_arg = va_arg(args, int);
-	arg = rtr_va_arg;
-	ft_putnbr_fd(arg, 1);
+	arg = (unsigned int)rtr_va_arg;
+
+	printunit(arg);
 	return (0);
+	
 }
